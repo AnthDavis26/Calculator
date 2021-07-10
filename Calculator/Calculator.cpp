1c6db9c3afdf598c6fd02ab27065e55ce322a46c
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2021-07-09 18:37:33 -0700

    Basic Implementation

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float a, b, result;
+	char op = -1;
+	
+	std::cout << "Enter your first number: ";
+	std::cin >> a;
+
+	std::cout << "Enter your second number: ";
+	std::cin >> b;
+
+	std::cout << "Enter your operand (+, -, *, or /): ";
+	std::cin >> op;
+
+	while (op != '+' && op != '-' && op != '*' && op != '/')
+		std::cin >> op;
+
+	switch (op)
+	{
+		case('-'): result = a - b; break;
+		case('*'): result = a * b; break;
+		case('/'): result = a / b; break;
+		default: result = a + b;
+	}
+
+	std::cout << "Result: " << result << std::endl;
 }
